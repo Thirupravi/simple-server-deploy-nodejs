dc7895d8b1eb1761805a4e44563d653786bdf5d3
Thirupravi <63950737+Thirupravi@users.noreply.github.com>
2021-02-24 17:49:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/receive', (req, res) => {
-  res.send('{"cmd":"browser_open","url":"file:///android_asset/index.html", "xpathinputbox":"/html/body/form/div/input","xpathbutton":"/html/body/form/div/button"}');
-})
+app.get("/receive", (req, res) => {
+  res.send(
+    '{"cmd":"browser_open","url":"https://m.facebook.com/", "xpathinputbox":"//*[@id="m_login_email"]","xpathbutton":"//*[@id="u_0_4_Ul"]/button"}'
+  );
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
